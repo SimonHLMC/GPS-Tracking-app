65bd3eefdbe7ec64a13145b3f5a3a72c84ac6dc0
Simon Bohnen <Simon Bohnen@ks01.rud-steuerungstechnik.de>
2026-04-20 12:43:25 +0200

    fix error

---
+++ vite.config.js
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+// https://vitejs.dev/config/
+export default defineConfig({
+    plugins: [react()],
+    base: '/gps-tracking-app/',
+    build: {
+        outDir: 'dist',
+        sourcemap: false,
+    }
+});
